d7f079a2a4dac47855b9d6fcf9ee64fe6fa20491
shazidhasanriam <shazid.riam@northsouth.edu>
2021-12-04 08:53:11 +0600

    Navigation

--- App.js
+++ App.js
@@ -1,22 +1,63 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// In App.js in a new project
 
-export default function App() {
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+
+import homeScreen from "./src/homeScreen";
+import explore from "./src/explore";
+import profile from "./src/profile";
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>My First React Native Application</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home";
+              return <Feather name={iconName} size={24} color="white" />;
+            } else if (route.name === "Explore") {
+              iconName = focused ? "explore" : "explore";
+              return <MaterialIcons name={iconName} size={24} color="white" />;
+            } else if (route.name === "Profile") {
+              iconName = focused ? "profile" : "profile";
+              return <AntDesign name={iconName} size={24} color="white" />;
+            }
+
+            // You can return any component that you like here!
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Home" component={homeScreen} />
+        <Tab.Screen name="Explore" component={explore} />
+        <Tab.Screen name="Profile" component={profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={homeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+export default MyTabs;
---
+++ src/explore.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>Explore</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+});
+
+export default HomeScreen;
